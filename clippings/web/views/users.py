efe99a01b8617011323c27c0943cc3732003acdf
yakimka <ss.yakim@gmail.com>
2024-09-14 23:59:41 +0300

    Add logout

--- clippings/web/views/users.py
+++ clippings/web/views/users.py
@@ -2,17 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from starlette.responses import RedirectResponse, Response
+from starlette.responses import Response
 
 if TYPE_CHECKING:
     from starlette.requests import Request
 
 
-async def login(request: Request) -> Response:
-    if request.user.is_authenticated:
-        next_url = request.query_params.get("next", "/")
-        return RedirectResponse(next_url)
-
+async def logout(request: Request) -> Response:  # noqa: U100
     return Response(
         content="Authentication required",
         status_code=401,
