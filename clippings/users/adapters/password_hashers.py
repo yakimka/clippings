71b94a079e1e670f4d55a09aaeb5f58194d20fe4
yakimka <ss.yakim@gmail.com>
2024-09-17 22:14:29 +0300

    Change hasher (#17)

--- clippings/users/adapters/password_hashers.py
+++ clippings/users/adapters/password_hashers.py
@@ -1,13 +1,41 @@
-import bcrypt
+import hashlib
+import hmac
+import os
 
 from clippings.users.ports import PasswordHasherABC
 
 
-class BcryptPasswordHasher(PasswordHasherABC):
+class PBKDF2PasswordHasher(PasswordHasherABC):
+    def __init__(self, iterations: int = 50_000, salt_length: int = 16):
+        self.iterations = iterations
+        self.salt_length = salt_length
+        self.algorithm = "sha1"
+        self.hash_length = 20
+
     def hash(self, password: str) -> str:
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
-        return hashed_password.decode("utf-8")
+        salt = os.urandom(self.salt_length)
+        hashed_password = hashlib.pbkdf2_hmac(
+            self.algorithm,
+            password.encode("utf-8"),
+            salt,
+            self.iterations,
+            dklen=self.hash_length,
+        )
+        return f"{salt.hex()}:{hashed_password.hex()}"
 
     def verify(self, password: str, hashed_password: str) -> bool:
-        return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
+        try:
+            salt_hex, hash_hex = hashed_password.split(":")
+            salt = bytes.fromhex(salt_hex)
+            stored_hash = bytes.fromhex(hash_hex)
+
+            new_hash = hashlib.pbkdf2_hmac(
+                self.algorithm,
+                password.encode("utf-8"),
+                salt,
+                self.iterations,
+                dklen=self.hash_length,
+            )
+            return hmac.compare_digest(new_hash, stored_hash)
+        except ValueError:
+            return False
